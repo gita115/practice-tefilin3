0685dc3dedf70ad8ec88312fc64fea817fc9e8a5
Devorah-Wachtel <devorahsphotos@gmail.com>
2025-06-12 11:25:48 +0300

    Form component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import EntryForm from './component/EntryForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <EntryForm></EntryForm>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
